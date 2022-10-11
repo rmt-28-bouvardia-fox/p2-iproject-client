be453f0ce636f0bfec103eac36825688f6ba7a37
LordFeexz <anandafiqri1403@gmail.com>
2022-10-11 23:55:11 +0700

    loginHandler

--- client/src/stores/user.js
+++ client/src/stores/user.js
@@ -1,4 +1,6 @@
 import { defineStore } from 'pinia'
+import axios from 'axios'
+import Swal from 'sweetalert2'
 
 export const useUserStore = defineStore('user', {
   state: () => ({
@@ -11,10 +13,38 @@ export const useUserStore = defineStore('user', {
       username:'',
       email:'',
       password:''
-    }
+    },
+    url:`http://localhost:3000`
   }),
   getters: {},
   actions: {
-    
+    async loginHandler(){
+      try {
+        const { data } = await axios({
+          method:'post',
+          url:`${this.url}/auth/login`,
+          data:this.loginData
+      })
+      localStorage.setItem('access_token',data.access_token)
+      localStorage.setItem('email',data.email)
+      localStorage.setItem('status',data.status)
+      localStorage.setItem('id',data.id)
+      this.isLogin = true
+      this.router.push('/')
+      this.loginData.email = ''
+      this.loginData.password = ''
+      Swal.fire({
+        icon:"success",
+        title:"gotcha",
+        timer:1500
+        })
+      } catch (err) {
+        Swal.fire({
+          icon:"error",
+          title:"Oops...",
+          text:`${err.response.data.message}`
+          })
+      }
+    }
   },
 })
