8f41d0f45b0ec72655231cc74b9559117d07fdb0
P4rley <adwihersa@gmail.com>
2022-10-11 15:57:26 +0700

    view homepage

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,110 @@
+<script></script>
+<template>
+  <div class="container">
+    <div class="top-wrapper">
+      <h1 class="logo">The Hacktiv Times</h1>
+      <ul class="link-wrapper">
+        <li>
+          <RouterLink to="/login" class="link user-btn">Login</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/register" class="link user-btn">Register</RouterLink>
+        </li>
+      </ul>
+    </div>
+    <div class="line"></div>
+    <div class="bottom-wrapper">
+      <ul class="link-wrapper">
+        <li>
+          <RouterLink to="/" class="link news-category">Sports</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/" class="link news-category">Business</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/" class="link news-category"
+            >Entertainment</RouterLink
+          >
+        </li>
+        <li>
+          <RouterLink to="/" class="link news-category">Health</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/" class="link news-category">Science</RouterLink>
+        </li>
+        <li>
+          <RouterLink to="/" class="link news-category">Technology</RouterLink>
+        </li>
+      </ul>
+      <div class="line2-top"></div>
+      <div class="line2-bottom"></div>
+    </div>
+  </div>
+</template>
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap");
+
+.container {
+  width: 100%;
+  padding: 1rem 8rem;
+}
+
+.top-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.line {
+  width: 100%;
+  margin: 1rem 0;
+  height: 1px;
+  background-color: rgba(128, 128, 128, 0.158);
+}
+
+.line2-top {
+  margin-top: 1rem;
+  height: 1px;
+  background-color: rgba(128, 128, 128, 0.5);
+}
+.line2-bottom {
+  width: 100%;
+  margin: 0.3rem 0;
+  height: 1px;
+  background-color: rgba(128, 128, 128, 0.5);
+}
+
+.logo {
+  font-family: "Great Vibes", cursive;
+}
+
+.link-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.link {
+  margin-left: 1rem;
+}
+
+.user-btn {
+  border: 1px solid black;
+  padding: 0.3rem 1.4rem;
+  background-color: #000;
+  border-radius: 5px;
+  color: #fff;
+  transition: all 0.4s ease-in-out;
+}
+
+.user-btn:hover {
+  background-color: transparent;
+  color: #000;
+}
+
+.news-category {
+  font-size: 12px;
+  color: #000;
+  font-weight: 400;
+}
+</style>
---
+++ client/src/views/HomePage.vue
@@ -0,0 +1,124 @@
+<script></script>
+<template>
+  <div class="container">
+    <div class="home-wrapper">
+      <div class="rows">
+        <!-- Card -->
+        <div class="card">
+          <div class="left">
+            <h3 class="card-title">Lorem ipsum dolor sit amet.</h3>
+            <div class="card-desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Repellendus perspiciatis laborum omnis aliquam non sed doloremque
+              amet soluta perferendis. Quae?
+            </div>
+          </div>
+          <div class="right">
+            <img
+              class="card-img"
+              src="https://images.unsplash.com/photo-1517245386807-bb43f82c33c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+              alt=""
+            />
+          </div>
+        </div>
+        <div class="card-line"></div>
+        <!-- Card -->
+        <div class="card">
+          <div class="left">
+            <h3 class="card-title">Lorem ipsum dolor sit amet.</h3>
+            <div class="card-desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Repellendus perspiciatis laborum omnis aliquam non sed doloremque
+              amet soluta perferendis. Quae?
+            </div>
+          </div>
+          <div class="right">
+            <img
+              class="card-img"
+              src="https://images.unsplash.com/photo-1517245386807-bb43f82c33c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+              alt=""
+            />
+          </div>
+        </div>
+        <div class="card-line"></div>
+        <!-- Card -->
+        <div class="card">
+          <div class="left">
+            <h3 class="card-title">Lorem ipsum dolor sit amet.</h3>
+            <div class="card-desc">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Repellendus perspiciatis laborum omnis aliquam non sed doloremque
+              amet soluta perferendis. Quae?
+            </div>
+          </div>
+          <div class="right">
+            <img
+              class="card-img"
+              src="https://images.unsplash.com/photo-1517245386807-bb43f82c33c4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+              alt=""
+            />
+          </div>
+        </div>
+        <div class="card-line"></div>
+      </div>
+    </div>
+    <div class="line"></div>
+    <div class="sidebar"></div>
+  </div>
+</template>
+<style scoped>
+.container {
+  min-height: 100vh;
+  padding: 0 8rem;
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+}
+
+.home-wrapper {
+  flex: 1.2;
+}
+
+.card {
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+}
+
+.left {
+  padding: 1rem;
+  margin-right: 50px;
+}
+
+.card-title {
+  font-weight: 600;
+  font-size: 1.25rem;
+}
+
+.card-desc {
+  font-weight: 300;
+  font-size: 0.75rem;
+  margin: 1rem 0;
+}
+
+.card-img {
+  width: 500px;
+  height: 300px;
+  object-fit: cover;
+}
+.card-line {
+  width: 100%;
+  height: 1px;
+  background-color: rgba(122, 122, 122, 0.235);
+  margin: 1rem 0;
+}
+.line {
+  min-height: 100vh;
+  width: 1px;
+  background-color: #000;
+  margin: 0 1rem;
+}
+.sidebar {
+  flex: 0.5;
+}
+</style>
