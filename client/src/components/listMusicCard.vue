0a24584af7593c9eed7284ab790f8084fed55a7f
LordFeexz <anandafiqri1403@gmail.com>
2022-10-11 23:02:46 +0700

    list music card

---
+++ client/src/components/listMusicCard.vue
@@ -0,0 +1,20 @@
+<script>
+
+</script>
+
+<template>
+    <div class="col-md-4 mt-md-5">
+        <div class="card card-custom">
+            <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                <img src="movie.imgURL" class="img-fluid card-img" />
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">title</h5>
+                <div class="container">
+                    <!-- <Button @click.prevent="movieDetail(movie.id)" :variant="'btn btn-primary'" :name="'See Detail'"
+                        :type="'button'"></Button> -->
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
