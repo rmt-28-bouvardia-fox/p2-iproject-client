180672cdb03744b79e584c31f6eb8a8619c6ef4d
pascal029 <pascalvirgiawan05@gmail.com>
2022-10-11 16:36:14 +0700

    retouch cards

--- client/src/components/ComicCards.vue
+++ client/src/components/ComicCards.vue
@@ -3,10 +3,16 @@
   export default {
     data(){
       return {
-        imgUrl : (this.comic.thumbnail.path + '.' + this.comic.thumbnail.extension) == `http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg` ? false : this.comic.thumbnail.path + '.' + this.comic.thumbnail.extension
+        imgUrl : (this.comic.thumbnail.path + '.' + this.comic.thumbnail.extension) == `http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg` ? false : this.comic.thumbnail.path + '.' + this.comic.thumbnail.extension,
+        price : this.comic.prices[0].price > 0 ?  this.comic.prices[0].price : 5
       }
     },
-    props : ['comic']
+    props : ['comic'],
+    computed : {
+      shownPrice(){
+        return this.price.toLocaleString("en-US", {style:"currency", currency:"USD"})
+      }
+    }
   }
 </script>
 
@@ -20,10 +26,9 @@
             <img :src="imgUrl" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">{{comic.title}}</h5>
-              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary mr-2 ms-3 me-3"><i class="fas fa-link"></i> Visit Site</a>
-              <a href="#" class="btn btn-primary"><i class="fab fa-github"></i> Github</a>
+              <h6 class="card-subtitle mb-2 text-muted">Price : {{ shownPrice }}</h6>
+              <a href="#" class="btn btn-primary mr-2 ms-3 me-3"><i class="fas fa-link"></i> See details</a>
+              <a href="#" class="btn btn-primary"><i class="fab fa-github"></i> Add to cart</a>
             </div>
         </div>
     </div>
