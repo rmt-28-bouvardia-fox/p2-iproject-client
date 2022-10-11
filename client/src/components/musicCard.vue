b5ef0affdeae44f750b9071edd3cb1a667d7954c
LordFeexz <anandafiqri1403@gmail.com>
2022-10-11 22:44:16 +0700

    music Card Component

---
+++ client/src/components/musicCard.vue
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<template>
+    <div class="card" style="width: 18rem;">
+        <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/182.webp" class="card-img-top"
+            alt="Sunset Over the Sea" />
+        <div class="card-body">
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                content.</p>
+        </div>
+    </div>
+</template>
