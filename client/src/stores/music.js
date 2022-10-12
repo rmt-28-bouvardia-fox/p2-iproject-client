2461da4339e388f610a6c531f108c8028c1e347b
LordFeexz <anandafiqri1403@gmail.com>
2022-10-12 10:16:47 +0700

    init music store

---
+++ client/src/stores/music.js
@@ -0,0 +1,13 @@
+import { defineStore } from 'pinia'
+import axios from 'axios'
+import Swal from 'sweetalert2'
+
+export const useMusicStore = defineStore('music',{
+    state: () => ({
+
+    }),
+    getters:{},
+    actions:{
+        
+    }
+})
