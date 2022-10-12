452b52e6c3d75611a75a5b5a7b2acfd0be76a828
LordFeexz <anandafiqri1403@gmail.com>
2022-10-12 10:06:19 +0700

    adjust

--- client/src/views/home.vue
+++ client/src/views/home.vue
@@ -1,12 +1,16 @@
 <script>
     import Sidebar from '../components/sidebar.vue'
+    import ListMusicCard from '../components/listMusicCard.vue'
     export default{
         components:{
-            Sidebar
+            Sidebar,
+            ListMusicCard
         }
     }
 </script>
 
 <template>
     <Sidebar />
+
+    <ListMusicCard />
 </template>
