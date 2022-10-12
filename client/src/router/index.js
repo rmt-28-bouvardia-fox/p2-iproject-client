4ebcd3b7f93f0e30f3fa49b886f1729cf45c8401
Zianurrahmani <nurrahmani.zian@gmail.com>
2022-10-12 12:18:29 +0700

    setup done

---
+++ client/src/router/index.js
@@ -0,0 +1,39 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from "../views/Home.vue"
+import Login from "../views/Login.vue"
+import Register from "../views/Register.vue"
+import MyList from "../views/MyList.vue"
+import Detail from "../views/Detail.vue"
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
+    },
+    {
+      path: '/mylist',
+      name: 'mylist',
+      component: MyList
+    },
+    {
+      path: '/detail',
+      name: 'detail',
+      component: Detail
+    }
+  ]
+})
+
+export default router
